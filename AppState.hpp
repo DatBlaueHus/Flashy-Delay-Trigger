0108bf1909787f9e4980172f85d21c7670a14663
Klaus Busse <klaus.busse@kilibee.com>
2024-07-27 06:45:12 +0200

    * Add version number

--- AppState.hpp
+++ AppState.hpp
@@ -6,7 +6,7 @@
 
 #define APPNAME "®" // Flashy Delay Trigger Logo
 #define VERSIONTEMPLATE "¯%s   ©" //version + KB Stamp
-#define VERSION "1.0.2"
+#define VERSION "1.0.3"
 
 // Port configuration =======================================================
 
